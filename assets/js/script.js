b14e2aa225121e065b18990ca270490131160f7e
Gabriel Gomes <124470408+Gowoba@users.noreply.github.com>
2024-12-27 22:29:08 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,29 @@
 const quizzes = {
     poo: {
         professor1: [
-            { question: "Com relação aos conceitos de Programação Orientada à Objetos é correto afirmar que (Unespar, 2023): <br>[_] Um construtor é implicitamente chamado no momento da instanciação do objeto.<br>[_] Construtor não é um método, seu único propósito é inicializar as variáveis de instância.<br>[_] Construtores não possuem especificadores de acesso, sendo públicos por default.<br>[_] Um Construtor pode ser chamado a qualquer instante utilizando a palavra-chave this. <br>[_] O Construtor da subclasse pode acessar o construtor da superclasse com uso da palavra-chave super. <br>[_] O polimorfismo paramétrico é utilizado para definir mais de um construtor para a classe. <br>[_] Uma classe que não possua um construtor declarado não poderá instanciar objetos. <br>[_] Uma classe que possua vários construtores poderá executar apenas um dester ao instancia um objeto.",
+            { question: "Em relação à linguagem Java, marque as afirmativas abaixo com (V) para verdadeira e (F) para falsa: <br>[_] Um construtor é implicitamente chamado no momento da instanciação do objeto.<br>[_] Construtor não é um método, seu único propósito é inicializar as variáveis de instância.<br>[_] Construtores não possuem especificadores de acesso, sendo públicos por default.<br>[_] Um Construtor pode ser chamado a qualquer instante utilizando a palavra-chave this. <br>[_] O Construtor da subclasse pode acessar o construtor da superclasse com uso da palavra-chave super. <br>[_] O polimorfismo paramétrico é utilizado para definir mais de um construtor para a classe. <br>[_] Uma classe que não possua um construtor declarado não poderá instanciar objetos. <br>[_] Uma classe que possua vários construtores poderá executar apenas um dester ao instancia um objeto.",
             choices: ["F F F F V F F V", "V V F F V F F F", "F V F F V F F F", "V V F F F V F V"], 
             answer: "V V F F V F F F" },
+
+            { question: "Com relação aos conceitos de Programação Orientada à Objetos é correto afirmar que (Unespar, 2023): <br>[_] Para uma classe ser considerada abstrata, todos os seus métodos devem ser abstrados. Em Java, para se definir uma classe abstrata deve-se utilizar a palavra-chave abstract no inicio de sua declaração; <br>[_] Poliformismo é o princípio pelo qual duas ou mais classes derivadas de uma mesma superclasse podem invocar métodos que têm a mesma assinatura mas comportamentos distintos, especializados para cada classe derivada, usando para tanto uma referência a um objeto do tipo da superclasse; <br>[_] O encapsulamento é alcançado por meio da definição da visibilidade pública aos atributos e métodos; <br>[_] Interface pode ser considerada como a forma com que um objeto se apresenta para outros, no que diz respeito aos seus atributos e métodos. Em Java, uma classe não pode implementar mais de uma interface; <br>[_] A herança é um mecanismo que permite que uma classe herde todo o comportamento e os atributos de outra classe. Em Java, pode-se implementar tanto a herança única quanto a herança múltipla. <br>",
+            choices: ["F V F F F", "F F V F V", "V V V F F", "F V F V F"], 
+            answer: "F V F F F" },
+
+            { question: "O que são atributos e métodos estáticos em Java?",
+            choices: ["Atributos e métodos estáticos pertencem a cada instância da classe e só podem ser acessados através de um objeto.", "Atributos e métodos estáticos pertencem à classe em si, podem ser acessados sem criar uma instância e não variam de acordo com o objeto.", "Atributos e métodos estáticos pertencem a interfaces e são usados exclusivamente para polimorfismo.", "Atributos estáticos pertencem à classe, enquanto métodos estáticos pertencem aos objetos criados."], 
+            answer: "Atributos e métodos estáticos pertencem à classe em si, podem ser acessados sem criar uma instância e não variam de acordo com o objeto." },
+
+            { question: "Quais são as restrições de métodos estáticos em Java?",
+            choices: ["Métodos estáticos não podem acessar diretamente atributos ou métodos não estáticos da classe e não podem usar as palavras-chave this ou super.", " Métodos estáticos podem acessar diretamente atributos e métodos não estáticos de uma classe.", " Métodos estáticos podem ser sobrescritos pelas subclasses para comportamentos específicos.", "Métodos estáticos precisam de uma instância da classe para serem chamados."], 
+            answer: "Métodos estáticos não podem acessar diretamente atributos ou métodos não estáticos da classe e não podem usar as palavras-chave this ou super." },
+
+            { question: "Qual a diferença entre erro e exceção em Java?",
+            choices: ["Exceções são situações que podem ser tratadas ou evitadas pelo programador, enquanto erros são condições mais graves, geralmente relacionadas a falhas de hardware ou problemas no ambiente de execução.", "Erros são condições anormais que podem ser corrigidas pelo programador, enquanto exceções são situações que não podem ser tratadas.", "Erros são eventos que ocorrem durante a execução do programa, e exceções são eventos que ocorrem durante a compilação do programa.", "Exceções e erros são ambos eventos que podem ser tratados de forma igual em Java, com a mesma abordagem."], 
+            answer: "Exceções são situações que podem ser tratadas ou evitadas pelo programador, enquanto erros são condições mais graves, geralmente relacionadas a falhas de hardware ou problemas no ambiente de execução." },
+            
+            { question: "(apenas pra eu copiar e colar mais rapido)",
+            choices: ["1", " 2", " 3", "4"], 
+            answer: "1" },
 
 
             { question: "O que é uma classe?", choices: ["Um molde para criar objetos", "Uma instância de um objeto", "Um tipo de variável", "Uma função que retorna um valor"], answer: "Um molde para criar objetos" }
